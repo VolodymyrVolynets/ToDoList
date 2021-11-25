05467e2269fa070ae2466144cf649e64a0d2c998
VolodymyrVolynets <vova.vvolynets@gmail.com>
2021-11-25 15:07:16 +0000

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Vova on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
